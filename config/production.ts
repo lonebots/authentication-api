ba1fe64c512a867d52c163bf3bf1b804639563df
lonebots <jishnusurajila@gmail.com>
2022-11-21 20:37:46 +0530

    config: production config template added

---
+++ config/production.ts
@@ -0,0 +1,6 @@
+export default {
+  port: 3000,
+  dbUri:
+    "mongodb+srv://<user_name>:<user_password>@<cluster_name>.hdp4pop.mongodb.net/<database_collection_name>?retryWrites=true&w=majority",
+  logLevel: "debug",
+};
