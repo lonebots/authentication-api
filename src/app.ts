f4f37fcb7d2d380fd6ff34b3b99522a370a0d359
lonebots <jishnusurajila@gmail.com>
2022-11-21 20:40:24 +0530

    feat:app.ts added route and db connection

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("hi jishnu is here")
+require("dotenv").config;
+import express from "express";
+import config from "config";
+import connect from "./utils/connect";
+import logger from "./utils/logger";
+import router from "./routes";
+
+const app = express();
+app.use(router);
+
+const port = config.get("port");
+
+app.listen(port, async () => {
+  logger.info(`App started at port:https://localhost:${port}`);
+  await connect();
+});
