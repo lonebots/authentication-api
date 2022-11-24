52d1b3fcf6173e5de1760835b1a60222ac754c0c
lonebots <jishnusurajila@gmail.com>
2022-11-24 14:50:07 +0530

    route:User password reset added

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import {
   createUserHandler,
+  forgotPasswordHandler,
   verifyUserHandler,
 } from "../controller/user.controller";
 import validateResource from "../middleware/validateResource";
-import { createUserSchema, verifyUserSchema } from "../schema/user.schema";
+import {
+  createUserSchema,
+  forgotPasswordSchema,
+  verifyUserSchema,
+} from "../schema/user.schema";
 
 const router = express.Router();
 
@@ -15,11 +20,18 @@ router.post(
   createUserHandler
 );
 
-export default router;
-
 // verify user
 router.post(
   "/api/users/verify/:id/:verificationCode",
   validateResource(verifyUserSchema),
   verifyUserHandler
 );
+
+// password reset
+router.post(
+  "/api/users/forgotpassword",
+  validateResource(forgotPasswordSchema),
+  forgotPasswordHandler
+);
+
+export default router;
