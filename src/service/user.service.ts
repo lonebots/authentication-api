d2626891cabd20f3721834c6a26dca320d8c4b28
lonebots <jishnusurajila@gmail.com>
2022-11-24 13:23:19 +0530

    feat:Finder user by Id added

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -4,3 +4,7 @@ import userModel, { User } from "../model/user.model";
 export async function createUser(input: Partial<User>) {
   return userModel.create(input);
 }
+
+export async function findUserById(id: string) {
+  return userModel.findById(id);
+}
