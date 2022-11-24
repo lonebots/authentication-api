9a8589f862da9699c9193265e6bdc703a4d0237e
lonebots <jishnusurajila@gmail.com>
2022-11-24 10:48:59 +0530

    addon:User registration api end point added

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import { createUserHandler } from "../controller/user.controller";
+import validateResource from "../middleware/validateResource";
+import { createUserSchema } from "../schema/user.schema";
 
 const router = express.Router();
+
+router.post(
+  "/api/users",
+  validateResource(createUserSchema),
+  createUserHandler
+);
 
 export default router;
