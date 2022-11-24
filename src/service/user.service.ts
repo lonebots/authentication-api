ece466edd27a0343f05aa77aa61f548c48b56629
lonebots <jishnusurajila@gmail.com>
2022-11-24 14:48:57 +0530

    feat:Find user by email service added

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -5,6 +5,12 @@ export async function createUser(input: Partial<User>) {
   return userModel.create(input);
 }
 
+// find user by id
 export async function findUserById(id: string) {
   return userModel.findById(id);
+}
+
+// find user by email
+export async function findUserByEmail(email: string) {
+  return userModel.findOne({ email });
 }
