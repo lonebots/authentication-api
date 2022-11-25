784a13d27cddb2369ce35bc1e4d351edd540101d
lonebots <jishnusurajila@gmail.com>
2022-11-25 17:43:47 +0530

    fix: App updated add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
-require("dotenv").config;
+require("dotenv").config();
 import express from "express";
 import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
 import router from "./routes";
+import deserializeUser from "./middleware/deserializeUser";
 
 const app = express();
 
 app.use(express.json()); // the order of the middleware matters.
+app.use(deserializeUser);// deserialize the users before using the routes.
 app.use(router);
+
 
 const port = config.get("port");
 
