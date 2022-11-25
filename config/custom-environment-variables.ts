3056382faffee519be4e2fa5048c13df96fdcc3e
lonebots <jishnusurajila@gmail.com>
2022-11-25 17:39:13 +0530

    config: custome env variables added(comment)

--- config/custom-environment-variables.ts
+++ config/custom-environment-variables.ts
@@ -1,3 +1,6 @@
+// create a .env folder in the root directory
+// add the following properties as key value pairs : ["ACCESS_TOKEN_PRIVATE_KEY", "ACCESS_TOKEN_PUBLIC_KEY", "REFRESH_PRIVATE_KEY","REFRESH_PUBLIC_KEY"]
+
 export default {
   accessTokenPrivateKey: "ACCESS_TOKEN_PRIVATE_KEY",
   accessTokenPublicKey: "ACCESS_TOKEN_PUBLIC_KEY",
