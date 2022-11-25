22d4ed44683d950328770da02351d731471f96cc
lonebots <jishnusurajila@gmail.com>
2022-11-25 17:42:55 +0530

    route: Authentication routes added

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import { createSessionHandler } from "../controller/auth.controller";
+import validateResource from "../middleware/validateResource";
+import { createSessionSchema } from "../schema/auth.schema";
 
 const router = express.Router();
 
 export default router;
+
+router.post(
+  "/api/sessions",
+  validateResource(createSessionSchema),
+  createSessionHandler
+);
